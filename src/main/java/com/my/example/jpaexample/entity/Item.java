e544b204e65602bcb59302309fe3224d94defc1c
y_h_s_0422 <iksrok@kyobobook.com>
2022-07-01 18:00:09 +0900

    [0629] Member, Order 엔티티 추가

--- src/main/java/com/my/example/jpaexample/entity/Item.java
+++ src/main/java/com/my/example/jpaexample/entity/Item.java
@@ -1,4 +1,17 @@
 package com.my.example.jpaexample.entity;
 
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class Item {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
 }
